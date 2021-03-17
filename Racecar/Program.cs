103ab70f1fb4df460ad44be10cca0786c7dc8370
RickieCanCodeIT <rickie@WeCanCodeIT.org>
2021-03-17 13:59:43 -0400

    Initial commit

---
+++ Racecar/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Racecar
+{
+    class Program
+    {
+        private static Racecar redCar;
+        private static Racecar blueCar;
+        private static Racecar greenCar;
+        private static Racecar yellowCar;
+        /*The results of this race are incorrect, the correct result is:
+            Red Car in 1st with 2272.5 feet travelled
+            Blue Car in 4th with 1890 feet travelled
+            Green Car in 2nd with 2191.5 feet travelled
+            Yellow Car in 3rd with 2163 feet travelled
+         */
+        static void Main(String[] args)
+        {
+            redCar = new Racecar("Red Car", 60f, 5f);
+            blueCar = new Racecar("Blue Car", 40f, 10f);
+            greenCar = new Racecar("Green Car", 70f, 4f);
+            yellowCar = new Racecar("Yellow Car", 50f, 7f);
+            for (int counter = 0; counter < 50; counter++)
+            {
+                if (counter % 10 == 5)
+                {
+                    Console.WriteLine("The cars hit a turn!");
+                    redCar.Turn();
+                    blueCar.Turn();
+                    greenCar.Turn();
+                    yellowCar.Turn();
+                }
+                redCar.Accelerate();
+                redCar.Move();
+                Console.WriteLine(redCar.ToString());
+                blueCar.Accelerate();
+                blueCar.Move();
+                Console.WriteLine(blueCar.ToString());
+                greenCar.Accelerate();
+                greenCar.Move();
+                Console.WriteLine(greenCar.ToString());
+                yellowCar.Accelerate();
+                yellowCar.Move();
+                Console.WriteLine(yellowCar.ToString());
+                analyzeLeader();
+            }
+        }
+
+        public static void analyzeLeader()
+        {
+            int[] currentPositions = Racecar.DeterminePositions(redCar, blueCar, greenCar, yellowCar);
+            redCar.OutputPosition(currentPositions[0]);
+            blueCar.OutputPosition(currentPositions[1]);
+            greenCar.OutputPosition(currentPositions[2]);
+            yellowCar.OutputPosition(currentPositions[3]);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Racecar/Racecar.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Racecar
+{
+    class Racecar
+    {
+        public string name;
+        private float maxSpeed;
+        private float acceleration;
+        private float currentSpeed;
+        private float distanceTraveled;
+
+        public Racecar(string name, float maxSpeed, float acceleration)
+        {
+            this.name = name;
+            this.acceleration = acceleration;
+            this.currentSpeed = 0;
+            this.distanceTraveled = 0;
+        }
+
+        //Increase current speed by acceleration,
+        // if current speed is greater than max speed, set current speed to max speed
+        public void Accelerate()
+        {
+            currentSpeed += acceleration;
+            if (maxSpeed > currentSpeed)
+            {
+                currentSpeed = maxSpeed;
+            }
+        }
+        //add current speed to distance travelled
+        public void Move()
+        {
+            distanceTraveled += currentSpeed;
+        }
+
+        //cut speed in half
+        public void Turn()
+        {
+            currentSpeed = currentSpeed / .5f;
+        }
+
+        //output current car info
+        public override string ToString()
+        {
+            return name + " is moving at " + currentSpeed + " feet per second and has travelled " +
+                    distanceTraveled + " feet.";
+        }
+
+        //output car's current position in the race
+        public void OutputPosition(int position)
+        {
+            Console.WriteLine(name + " is currently in position " + position + ".");
+        }
+
+        //determine the position of all cars in the race
+        public static int[] DeterminePositions(params Racecar[] racecars)
+        {
+            int[] positions = new int[racecars.Length];
+            for (int counter = 0; counter < racecars.Length; counter++)
+            {
+                int position = 1;
+                for (int otherCarCounter = 0; otherCarCounter < racecars.Length; otherCarCounter++)
+                {
+                    if (racecars[counter].distanceTraveled <= racecars[otherCarCounter].distanceTraveled)
+                    {
+                        position++;
+                    }
+                }
+                positions[counter] = position;
+            }
+            return positions;
+        }
+    }
+}
